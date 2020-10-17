f43486def5610018ab97886a478025ecc5023f92
enteabstracto <jean.perezb@udea.edu.co>
2020-10-17 17:39:35 -0500

    Tuve que cambiar la funcion y borrar el codigo, sorry un fracaso mas.

--- Parcial1QT/main.cpp
+++ Parcial1QT/main.cpp
@@ -2,7 +2,7 @@
 # include <math.h>
 
 using namespace std;
-
+int funcioncañosnofensivo(int altura, int angulos, int posicioni);
 int main()
 {
 int op=-1;
@@ -18,28 +18,31 @@ while(op!=0){
     cin >>H;
     cin>>angulo;
     cin>>x1;
-    cout<< "segun nuestros parametros: DD"<<endl;
+    cout<< "segun nuestros parametros inicie con altura, lugo angulo y por ultimo posicion: DD"<<endl;
     while(i<3){
        cin>>DD;
        parametrosDD[i]=DD;
-       i++;
+       i+=1;
     }
-    int Parametros[3]={H,angulo,x1};
+    int Parametros[4]={funcioncañosnofensivo(H, angulo, x1)};
     switch (op) {
 
     case 1:{
     break;}
 
     case 2:{
-
+        int velDD= cos(angulo);
+        int tiempoDD=(Parametros[1]-parametrosDD[1])/velDD;
+        cout<<"la el tiempo de choque de el disparo es de : "<<tiempoDD<<endl;
+        cout<<"la velocidad con al que choca el ddisparo a el cañon ofensivo es de: "<<velDD<<endl;
         break;}
     }
 }
     return 0;}
-int funcioncañosnofensivo(int arreglo[]){
-    int velx=cos(arreglo[1]);
-    int posx=arreglo[2]+(velx*2.5);             //se toma el 2,5 debido a lo que tarda en llegar la informacion
-    int vely=sin(arreglo[1])-(9.8*2.5);
-    int posy=arreglo[0]+(vely*2.5)-(0.5*9.8*2.5*2.5);
-    return velx, posx, vely, posy;
-}
+int funcioncañosnofensivo(int altura, int angulos, int posicioni){
+    int velx=cos(angulos);
+    int posx=posicioni+(velx*2.5);             //se toma el 2,5 debido a lo que tarda en llegar la informacion
+    int vely=sin(angulos)-(9.8*2.5);
+    int posy=altura+(vely*2.5)-(0.5*9.8*2.5*2.5);
+    return velx,posx,vely,posy;
+    }
