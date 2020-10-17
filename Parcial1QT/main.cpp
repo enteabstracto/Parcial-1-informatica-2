447c9fabe77cad4d3d7ecb7a8da087703b269fcc
enteabstracto <jean.perezb@udea.edu.co>
2020-10-17 16:14:44 -0500

    Inicio de el codigo

---
+++ Parcial1QT/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int op=-1;
+while(op!=0){
+    cout<<" 1 para Generar disparos que comprometan la integridad del cañón defensivo";
+    cout<<" 2 para Generar disparos que comprometan la integridad del cañón ofensivo.";
+    cout<<" 3 para generar disparos defensivos sin importar si el cañón ofensivo pueda ser destruido.";
+    cout<<" 4 para generar disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos.";
+    cin>>op;
+    switch (op) {
+
+    case 1:{
+
+    break;}
+    }
+}
+    return 0;
+}
